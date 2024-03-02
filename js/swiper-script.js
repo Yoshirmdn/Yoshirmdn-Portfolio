6f24a4d31b3d7075c450dbbe995bb26339cd7ab9
Rio Ragil Ramdani <rioragilramdani7@gmail.com>
2024-03-02 22:08:12 +0700

    i need money

---
+++ js/swiper-script.js
@@ -0,0 +1,35 @@
+$(function () {
+    var swiperTestimonials = new Swiper('.swiper', {
+        autoplay: {
+            delay: 3000
+        },
+        speed: 2000,
+        slidesPerView: 3,
+        slidesPerGroup: 1,
+        spaceBetween: 10,
+        loop: true,
+        grabCursor: true,
+        breakpoints: {
+            // when window width is >= 360px
+            360: {
+                slidesPerView: 1,
+            },
+            // when window width is >= 768px
+            768: {
+                slidesPerView: 2,
+            },
+            // when window width is >= 1024px
+            1024: {
+                slidesPerView: 3,
+            }
+        },
+        // If we need pagination
+        pagination: {
+            enabled: false,
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+    });
+
+});
